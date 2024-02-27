0f80901991a2fb1707bfe857c6b69cd7502ae5cc
NN <135822376+nick6008@users.noreply.github.com>
2024-02-27 23:56:32 +0300

    Add files via upload

--- GitHubLab/About.js
+++ GitHubLab/About.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
+import { Linking } from 'react-native';
 
 const About = () => {
   return (
     <div>
       <h1>Ссылки</h1>
       <ul>
-        <li>https://reactnativedev.ru/rn/#_3</li>
+        <li><Link to="https://reactnativedev.ru/rn">https://reactnativedev.ru/rn/</Link></li>
+        <li><Link to="https://www.jscamp.app/ru/docs/reactnative00">www.jscamp.app/ru/docs/reactnative00</Link></li>
       </ul>
     </div>
   );
